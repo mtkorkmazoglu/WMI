670a8d05dde5c21c9b2c896dfb38100e793c6071
Muhammet Korkmazoğlu <87977699+mtkorkmazoglu@users.noreply.github.com>
2023-10-02 13:41:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(16, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SystemSku";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(15, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ProcessorFamily";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(13, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CurrentClockSpeed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(13, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ProcessorStatus";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(15, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "DiskSize";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(15, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "FreeSpace";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(238, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(339, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(238, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(339, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(238, 100);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(339, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(238, 137);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(339, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(238, 173);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(339, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(238, 213);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(339, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(15, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "RamSize";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(238, 251);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(339, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(238, 285);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(339, 20);
+            this.textBox9.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(16, 285);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "CpuUsage";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(16, 323);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 20);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "MemoryUsage";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(238, 323);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(339, 20);
+            this.textBox10.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetAccount();
+            GetProcessorInfo();
+            GetDiskInfo();
+            GetRamInfo();
+            GetCpuUsage();
+            GetMemoryUsage();
+        }
+
+        private void GetAccount()
+        {
+
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_ComputerSystem");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+                string systemSku = provider["SystemSkuNumber"].ToString();
+                textBox2.Text = systemSku.ToString();
+            }
+        }
+
+        private void GetProcessorInfo()
+        {
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_Processor");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+                int procFamily = Convert.ToInt16(provider["Family"]);
+                int procSpeed = Convert.ToInt32(provider["CurrentClockSpeed"]);
+                string procStatus = provider["status"].ToString();
+
+                textBox3.Text = procFamily.ToString();
+                textBox4.Text = procSpeed.ToString() + " " + "Mhz";
+                textBox5.Text = procStatus.ToString();
+            }
+        }
+        private void GetDiskInfo()
+        {
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_LogicalDisk");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+
+                string diskSize = provider["Size"].ToString();
+                textBox6.Text = Math.Round(Convert.ToInt64(diskSize) / Math.Pow(1024, 3)) + " GB";
+
+                string freeSPace = provider["FreeSpace"].ToString();
+                textBox7.Text = Math.Round(Convert.ToInt64(freeSPace) / Math.Pow(1024, 3)) + " GB";
+            }
+        }
+
+        private void GetRamInfo()
+        {
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_PhysicalMemory");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+                string ramSize = provider["Capacity"].ToString();
+                textBox8.Text = Math.Round(Convert.ToInt64(ramSize) / Math.Pow(1024, 3)) + " GB";
+            }
+        }
+
+        private void GetCpuUsage()
+        {
+
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_Processor");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+                string systemSku = provider["LoadPercentage"].ToString();
+                textBox9.Text = systemSku.ToString();
+            }
+        }
+
+        private void GetMemoryUsage()
+        {
+
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_Process");
+            var providers = wmi.GetInstances();
+            foreach (var provider in providers)
+            {
+                string memoryUsage = provider["WorkingSetSize"].ToString();
+                textBox10.Text = Math.Round(Convert.ToInt64(memoryUsage) / Math.Pow(1024, 1)) + " KB";
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-9A7HLLK;Initial Catalog=TestDb;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into Table_1 (SystemSku, ProcessorFamily, CurrentClockSpeed, ProcessorStatus, DiskSize, FreeSpace, RamSize, CpuUsage, MemoryUsage) values (@SystemSku, @ProcessorFamily, @CurrentClockSpeed, @ProcessorStatus, @DiskSize, @FreeSpace, @RamSize, @CpuUsage, @MemoryUsage)", conn);
+            //cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@SystemSku", (textBox2.Text));
+            cmd.Parameters.AddWithValue("@ProcessorFamily", (textBox3.Text));
+            cmd.Parameters.AddWithValue("@CurrentClockSpeed", (textBox4.Text));
+            cmd.Parameters.AddWithValue("@ProcessorStatus", (textBox5.Text));
+            cmd.Parameters.AddWithValue("@DiskSize", (textBox6.Text));
+            cmd.Parameters.AddWithValue("@FreeSpace", (textBox7.Text));
+            cmd.Parameters.AddWithValue("@RamSize", (textBox8.Text));
+            cmd.Parameters.AddWithValue("@CpuUsage", (textBox9.Text));
+            cmd.Parameters.AddWithValue("@MemoryUsage", (textBox10.Text));
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Successfull");
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetAccount();
+            GetProcessorInfo();
+            GetDiskInfo();
+            GetRamInfo();
+            GetCpuUsage();
+            GetMemoryUsage();
+            button1_Click(sender, e);
+        }
+    }
+}
+
